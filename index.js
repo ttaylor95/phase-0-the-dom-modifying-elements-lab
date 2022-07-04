f102db3fbb22b1356f2c5d8f08ec310979bea211
Teanna Taylor <teannamtaylor95@gmail.com>
2022-07-04 01:21:44 -0700

    commit tree 1d7f5785ea40336c52ac44ac7c3f7fdeecdbb398

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+// no loner has DOM node 'Main'
+const element = document.getElementById('main');
+element.remove();
+
+
+// has a newHeader variable 
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = "Teanna is the champion"
+//new header variable points to id 
+
